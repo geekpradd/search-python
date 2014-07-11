5938b20b11c3b0192ee0b03ff2f15146b211ed7a
geekpradd <pradd@outlook.com>
2014-07-11 06:43:32 -0700

    Create google.py

---
+++ google.py
@@ -0,0 +1,38 @@
+'''Google Search Module For Windows Explorer By Pradd
+
+Created By : geekpradd
+License : GPL V2 
+'''
+
+
+
+import sys, os
+
+if len(sys.argv) <2:
+
+     print usage
+     
+     sys.exit(0)
+     
+filename = sys.argv[1].split("\\")[-1]
+
+filenames = filename.split('.')[0]
+
+file = filenames.split()
+
+total_name = ''
+
+for ty in file:
+
+     if ty == file[-1]:
+     
+          total_name+=ty
+          
+     else:
+          total_name+= ty + '+'
+          
+          
+link = "http://www.google.com/search?q="
+
+
+os.system("start " + link + total_name)
