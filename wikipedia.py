3d2668719cb7f03956ae63a6389decc8709b80bb
geekpradd <pradd@outlook.com>
2014-07-11 06:47:02 -0700

    Create wikipedia.py

---
+++ wikipedia.py
@@ -0,0 +1,43 @@
+'''Wikipedia Search Module By Pradd 
+
+Created By : geekpradd
+
+License : GPL V2
+
+'''
+
+
+
+import sys, os
+
+usage = "To search on wikipedia , enter command like this python wikipedia.py [search term]"
+
+if len(sys.argv) <2:
+
+     print (usage)
+     
+     sys.exit(0)
+     
+filename = sys.argv[1].split("\\")[-1]
+
+filenames = filename.split('.')[0]
+
+file = filenames.split()
+
+total_name = ''
+
+for ty in file:
+
+     if ty == file[-1]:
+     
+          total_name+=ty
+          
+     else:
+     
+     
+          total_name+= ty + '+'
+          
+link = "http://en.wikipedia.org/w/index.php?search="
+
+
+os.system("start " + link + total_name)
